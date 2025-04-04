f775ec3a82cea9fe54f4d5dacf0f102ce10d3803
Roman Tsapko <roma.tsapko.01@mail.ru>
2025-04-04 23:09:55 +0300

    change basetest

--- src/main/java/core/base/BaseTest.java
+++ src/main/java/core/base/BaseTest.java
@@ -3,16 +3,34 @@ package core.base;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class BaseTest {
 
-    @BeforeEach
-    public void setup() {
-        Configuration.browser = "Chrome";
+    @BeforeAll
+    public static void setup() {
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "128.0";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://test-remservice.aqsi.ru/";
         Configuration.timeout = 17000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
